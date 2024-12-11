3e6432199b28009cf15a1de2af147c11245cf8d2
Sylvester Khwanyane <145204771+Khwanyane@users.noreply.github.com>
2024-12-11 11:28:12 +0200

    Update init_app.dart

--- lib/init_app.dart
+++ lib/init_app.dart
@@ -6,10 +6,10 @@ import 'package:asignment_2/services/user_services.dart';
 import '../../routes/routes.dart';
 
 class InitializingApp {
-  static const String appID = '952992C7-CEBC-D21B-FF0E-7526A030C200';
-  static const String androidApiKey = 'EE32ED64-1AE2-45C1-B001-E2ADAFC62E8F';
-  static const String iosApiKey = '7BF49A41-03B4-4716-A75E-5C8972E48AAF';
-  static const String jsKey = 'BAB0DDAA-1514-476D-AA9A-54D8B614A4C2';
+  static const String appID = '4D67F5DF-AF27-4C3A-ADBA-5A140D3EE747';
+  static const String androidApiKey = 'E444857B-DCF6-4E3A-9986-E4309B4A3256';
+  static const String iosApiKey = 'AECC6CDF-5D10-49AF-BBC1-94D98B6746BB';
+  static const String jsKey = 'D1A9DBB4-74F9-486B-80E0-FF29D35ACE0B';
 
   static void initializeApp(BuildContext context) async {
     await Backendless.initApp(
